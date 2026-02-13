60afaba0e4aa7c050b7d71c5e999c9e543390c92
luishv22 <wychox3@gmail.com>
2026-02-12 23:52:57 -0600

    Create numero_mayor.py
    Agrego programa que obtiene el número mayor de 5 valores usando función

---
+++ numero_mayor.py
@@ -0,0 +1,21 @@
+#Programa que recibe 5 números
+#y regresa el mayor
+
+def obtener_mayor():
+    numeros = []
+
+# pedir 5 números
+    for i in range (5):
+        num = int(input(f"Ingrese el número {i +1}: "))
+        numeros.append(num)
+
+# encontrar el número mayor
+    mayor = numeros[0]
+    for n in numeros:
+        if n > mayor:
+            mayor = n
+    
+    return mayor
+
+resultado = obtener_mayor() 
+print(f"El número mayor es: {resultado}")
