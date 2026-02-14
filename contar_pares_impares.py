e9f38d3a19b3bf5beee086be2d67f9a3a0412fe6
luishv22 <wychox3@gmail.com>
2026-02-13 21:41:33 -0600

    Add function to count even and odd numbers
    Agrego programa que cuenta números pares e impares usando función

---
+++ contar_pares_impares.py
@@ -0,0 +1,27 @@
+#Programa que recibe 5 números 
+#y regresa dos valores: pares e impares
+
+def contar_pares_impares():
+    numeros = []
+    pares =  0
+    impares = 0
+
+# pedir 5 números
+    for i in range(5):
+        num = int(input(f"Ingrese el número {i + 1}: "))
+        numeros.append(num)
+
+# contar pares e impares
+    for n in numeros:
+        if n % 2 == 0:
+            pares += 1
+        else:
+            impares += 1
+            
+    return pares, impares
+
+# programa principal
+pares, impares = contar_pares_impares()
+
+print(f"Números pares: {pares}")
+print(f"Números impares: {impares}")
